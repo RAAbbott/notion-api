cde7ea88cdc734dbcc25808fa7986dddf256b741
RAAbbott <a.abbott2596@gmail.com>
2021-06-16 09:52:01 -0600

    Successfully adding the influencer into notion, need to get the firebase data now. I will run this once to get influencers into Notion, then set up a firebase function that will add each influencer/follower on creation in Paktola

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ require('dotenv').config()
 // This will use the Notion API to create a dashboard for Paktola that lists
 // all influencers, users and meetings. For management and quick looks at the database
 
+// EDIT: This is just some playground code for now, I will add a firebase function that adds the influencer/follower to notion
+// when their account is created. This current code isn't efficient for long term at all, I'l prob use it
+// just the first time to get all the current influencers/followers into Notion
+
 const notion = new Client({
     auth: process.env.NOTION_TOKEN
 })
@@ -25,12 +29,31 @@ const follower = {
 
 }
 
-const getNewInfluencers = async () => {
+const getUniqueInfluencers = async () => {
     // This influencer array will come from firebase
     const influencers = [
-        {id: '1231323'},
-        {id: '12312323'},
-        {id: '13231323'},
+        {
+            name: 'alex',
+            email: 'safldjsaf@fdlasfjds.com',
+            feeTier: 'bronze',
+            flatRate: 122,
+            hourlyRate: 148,
+            paktolaLink: 'fdlkjsaklfjfdklsajf',
+            tz: 'America/Denver',
+            username: 'alexabbott',
+            id: '12312lkjlksfdjlkj21'
+        },
+        {
+            name: 'alexs',
+            email: 'dfdf@fdlasfjds.com',
+            feeTier: 'bronze',
+            flatRate: 121232,
+            hourlyRate: 1232348,
+            paktolaLink: 'fdlkjsasdafdsafdsfdklfjfdklsajf',
+            tz: 'America/Denver',
+            username: 'alexabddbott',
+            id: '1234567894564sd'
+        }
     ]
 
     // Get existing influencer objects in Notion and map out to array of IDs
@@ -38,16 +61,17 @@ const getNewInfluencers = async () => {
         database_id: process.env.DB_ID,
     })).results.map(obj => obj.properties.ID.rich_text[0]?.plain_text)
 
-    // Compare IDs to influencers from Firebase
-    console.log(existingInfluencers)
-
-    return []
+    // Compare IDs to influencers from Firebase and return unique influencers only
+    return existingInfluencers.length ? [influencers.filter(obj => !existingInfluencers.includes(obj.id))]: influencers
 }
 
 const run = async () => {
+    const promises = (await getUniqueInfluencers()).map(obj => notion.pages.create(createInfluencerPayload(obj)))
+    Promise.all(promises).then(values => {
+        console.log('All objects added!')
+    })
     // console.log(await notion.users.list())
     // const test = await notion.pages.create(createInfluencerPayload(influencer))
-    getNewInfluencers()
     // console.log(test)
 }
 
